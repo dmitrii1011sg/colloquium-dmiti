60eefcb9fda55acb0e61d9232d73cbe1ac173ac6
Gorshkov Dmitrii <dmitrii1011sg@gmail.com>
2026-04-19 18:07:03 +0300

    feat: dynamic form for funcs

--- src/ui/components/modules/NModule.py
+++ src/ui/components/modules/NModule.py
@@ -1,16 +1,33 @@
 import flet as ft
+from core.modules.ADD_1N_N import ADD_1N_N
+from core.modules.COM_NN_D import COM_NN_D
+from core.modules.MUL_ND_N import MUL_ND_N
+from core.modules.MUL_Nk_N import MUL_Nk_N
+from core.modules.NZER_N_B import NZER_N_B
+from ui.components.forms.dynamic_func_form import DynamicFuncForm
 
 
 class NModule(ft.Column):
     def __init__(self):
         super().__init__()
         self.expand = True
+
+        self.registry = {
+            "N-1: Сравнение чисел (COM_NN_D)": COM_NN_D,
+            "N-2: Проверка на ноль (NZER_N_B)": NZER_N_B,
+            "N-3: Добавить 1 числу (ADD_1N_N)": ADD_1N_N,
+            "N-6: Умножение на цифру (MUL_ND_N)": MUL_ND_N,
+            "N-7: Умножение на 10^k (MUL_Nk_N)": MUL_Nk_N,
+        }
+
         self.controls = [
             ft.Text(
                 "Модуль: Натуральные числа (N)", size=28, weight=ft.FontWeight.BOLD
             ),
             ft.Text(
-                "Здесь будет логика сложения, вычитания, умножения и т.д.",
+                "Выберите функцию из списка ниже для выполнения операции.",
                 color=ft.Colors.GREY_400,
             ),
+            ft.Divider(height=20, color=ft.Colors.TRANSPARENT),
+            DynamicFuncForm(functions_registry=self.registry, module_name="Модуль N"),
         ]
