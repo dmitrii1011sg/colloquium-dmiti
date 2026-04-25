e58fa604ecd80ae9f5ffabccf9bd18e3d50c907a
AlexandrZhukovLETI <lyohabunkerao@gmail.com>
2026-04-25 13:05:43 +0300

    Update MOD_NN_N.py

--- src/core/modules/MOD_NN_N.py
+++ src/core/modules/MOD_NN_N.py
@@ -1,8 +1,8 @@
 from core.base.Natural import Natural
 from core.modules.COM_NN_D import COM_NN_D
 from core.modules.DIV_NN_N import DIV_NN_N
-from core.modules.SUB_NDN_N import SUB_NDN_N
-from core.modules.MUL_Nk_N import MUL_Nk_N
+from core.modules.MUL_NN_N import MUL_NN_N
+from core.modules.SUB_NN_N import SUB_NN_N
 # Жуков Александр 5381
 
 def MOD_NN_N(a: Natural, b: Natural) -> Natural:
@@ -28,15 +28,8 @@ def MOD_NN_N(a: Natural, b: Natural) -> Natural:
     if COM_NN_D(a, b) == 1:
         return a
 
-    quot = DIV_NN_N(a, b)
-    result = a
-
-    for power in range(quot.length):
-        digit = quot.digits[power]
-        if digit == 0:
-            continue
-        digit_num = Natural([digit], need_reverse=False)
-        divisor_shifted = MUL_Nk_N(b, Natural.from_int(power))
-        result = SUB_NDN_N(result, divisor_shifted, digit_num)
+    res_div = DIV_NN_N(a, b)
+    res_mul = MUL_NN_N(b, res_div)
+    result = SUB_NN_N(a, res_mul)
 
     return result
